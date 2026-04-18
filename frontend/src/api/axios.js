e8e2d6c5d9b8217fd3b48971b23b64c38c657415
Nicolo Veneziale <nicoloveneziale@gmail.com>
2026-04-18 19:28:25 +0100

    Added report function, refactored frontend so its more modular

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://sketchle.onrender.com', 
+    baseURL: 'https://sketchle.onrender.com/api', 
     headers: {
         'Content-Type': 'application/json'
     }
