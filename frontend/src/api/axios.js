0398fd3e4ef3ae0184d376af177cbbd9a9cd8606
Nicolo Veneziale <nicoloveneziale@gmail.com>
2026-04-19 12:12:35 +0100

    Changed server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://sketchle.onrender.com/api', 
+    baseURL: 'sketchle.up.railway.app/api', 
     headers: {
         'Content-Type': 'application/json'
     }
